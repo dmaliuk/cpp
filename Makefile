aab5be63dd877e494e2d76ed022fba7511d5c46f
Dmitry M <dmaliuk@dmitrys-mbp.gateway.pace.com>
2016-12-04 15:50:09 -0600

    working

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
 CPPFLAGS = -std=c++1y -I /usr/local/Cellar/boost/1.60.0_2/include
-CPPFLAGS1 = -std=c++1y -I /Users/dmaliuk/repos/nnvm/include/dmlc
 
 all: main
 
@@ -10,4 +9,4 @@ main.pp: main.cpp
 	g++ -E $(CPPFLAGS) -o $@ $<
 
 main1: main1.cpp
-	g++ $(CPPFLAGS1) -o $@ $<
+	g++ $(CPPFLAGS) -o $@ $<
--- main1.cpp
+++ main1.cpp
@@ -1,4 +1,5 @@
-#include <any.h>
+#include <boost/any.hpp>
+#include <boost/unordered_map.hpp>
 #include <cassert>
 #include <vector>
 #include <map>
@@ -11,7 +12,9 @@
 #include <unordered_map>
 #include <array>
 
+////////////////////////////////////////////////////////////
 // metaprogramming stuff
+////////////////////////////////////////////////////////////
 template<class... T>
 using void_t = void;
 
@@ -23,7 +26,9 @@ template<class M> struct IsMap: std::false_type {};
 template<class K, class V> struct IsMap<std::map<K, V>>: std::true_type {};
 template<class K, class V> struct IsMap<std::unordered_map<K, V>>: std::true_type {};
 
-// IO stuff
+////////////////////////////////////////////////////////////
+// Print stuff
+////////////////////////////////////////////////////////////
 template<class T>
 void write(std::ostream& out, T const& value);
 
@@ -72,6 +77,59 @@ void write(std::ostream& out, T const& value)
   writer_helper(out, value, (void*)nullptr);
 }
 
+////////////////////////////////////////////////////////////
+// Type manager stuff
+////////////////////////////////////////////////////////////
+template<class T>
+void write_any(std::ostream& out, boost::any const& value)
+{
+  auto const& v = boost::any_cast<T const&>(value);
+  write(out, v);
+};
+
+class TypeManager {
+ private:
+  
+  using WriterFunc = void (*)(std::ostream&, boost::any const&);
+  
+  boost::unordered_map<boost::typeindex::type_index, WriterFunc> type2func;
+
+  template<class T>
+  void RegisterType() {
+    auto typeId = boost::typeindex::type_id<T>();
+    if (type2func.count(typeId) == 0) {
+      type2func.insert(std::make_pair(typeId, write_any<T>));
+    }
+  };
+
+  TypeManager() {
+    RegisterType<int>();
+    RegisterType<std::string>();
+    RegisterType<double>();
+    RegisterType<std::vector<std::string>>();
+    RegisterType<std::vector<double>>();
+    RegisterType<std::map<std::string, double>>();
+  }
+ public:
+      
+  static TypeManager& Global() {
+    static TypeManager manager;
+    return manager;
+  };
+
+  WriterFunc GetWriter(boost::typeindex::type_index const& typeId) const {
+    auto it = type2func.find(typeId);
+    assert(it != type2func.end());
+    return it->second;
+  };
+};
+
+void PrintAny(std::ostream& out, boost::any const& value)
+{
+  auto& typeId = value.type();
+  auto func = TypeManager::Global().GetWriter(typeId);
+  func(out, value);
+};
 
 int main()
 {
@@ -80,4 +138,9 @@ int main()
   write(std::cout, m);
   std::cout << std::endl;
 
+  auto f = TypeManager::Global().GetWriter(boost::typeindex::type_id<int>());
+
+  boost::any v{m};
+  std::cout << "Any print:\n";
+  PrintAny(std::cout, v);
 }
